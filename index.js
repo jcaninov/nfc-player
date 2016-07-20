54fddb7761e1fd4313f71d44a978e2ce343a897a
Javier Canino <Javier Canino>
2016-07-20 14:32:15 +0200

    Herencia simple por instanciacion de parent. Flujo cerrado

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
+//https://jsfiddle.net/71osymop/
+
 var Track = require('./lib/track.js');
 var PlayerController = require('./lib/players.js');
+var Enums = require('./lib/enums.js');
+var Rockola = require('./lib/rockola.js');
+var mpd = require('mpd'),
+    cmd = mpd.cmd;
 
+var mpdClient = mpd.connect({
+  port: 6600,
+  host: 'localhost',
+});
 
+mpdClient.on('ready', function() {
+  console.log("ready");
+  //debugger;
+  RunRockola();
+});
 
-var tracks = [ new Track("sample", 'Sample Music/sample.wav') ];
+mpdClient.on('system', function(name) {
+  console.log("update", name);
+});
+
+mpdClient.on('system-player', function() {
+  mpdClient.sendCommand(cmd("status", []), function(err, msg) {
+    if (err) throw err;
+    console.log(msg);
+  });
+});
+
+/*
+
+var tracks = [ new Track("sample", 'Sample Music/sample.wav','','',Enums.trackType.audio) ];
 
 var rock = new PlayerController();
 
  
 rock.play(tracks[0]);
+
+rock.pause();
+
+rock.stop();
+*/
+
+
+function RunRockola(){
+  //.replace(/\\/g,"\\\\");
+  var tracks = [ new Track("sample", 'Sample Music/sample.wav') ];
+  //var tracks = [ new Track("sample", "sample.wav") ];
+  //var tracks = [ new Track("This is music"),  new Track("oooo","http://ooo.com")];
+  //Logger.echo(JSON.stringify(tracks[0], null, 2));
+
+  var rock = new Rockola();
+  rock.addTrack(tracks);
+
+  rock.play();
+
+  //rock.pause();
+  //rock.next();
+  return;
+}
+
