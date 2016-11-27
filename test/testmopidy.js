b6eb18c0f4918a7d5e80294c29e65755398c3bf0
Javier Canino Vázquez <jcaninov@gmail.com>
2016-11-27 11:13:19 +0100

    Mopidy.js lib

--- test/testmopidy.js
+++ test/testmopidy.js
@@ -1,22 +1,59 @@
 var AudioPlayer = require('../lib/audioplayer.js'),
 	Track = require('../lib/track.js'), 
-	mpd = require('mpd');
+	mpd = require('mpd'),
+	Mopidy = require('mopidy');
 
-var mpdClient = mpd.connect({
-  port: 6600,
-  host: 'localhost',
-});
+var get = function (key, object) {
+    return object[key];
+};
 
-console.log("OK => Mpd connected");
+var printTypeAndName = function (model) {
+    console.log(model.__model__ + ": " + model.name);
+    // By returning the playlist, this function can be inserted
+    // anywhere a model with a name is piped in the chain.
+    return model;
+};
 
-var AudioPlayer = new AudioPlayer(mpdClient, mpd, null);
+var trackDesc = function (track) {
+    return track.name + " by " + track.artists[0].name +
+        " from " + track.album.name;
+};
 
-console.log("OK => New Audioplayer instance created");
+var printNowPlaying = function () {
+    // By returning any arguments we get, the function can be inserted
+    // anywhere in the chain.
+    var args = arguments;
+    return mopidy.playback.getCurrentTrack()
+        .then(function (track) {
+            console.log("Now playing:", trackDesc(track));
+            return args;
+        });
+};
 
-mpdClient.on('ready', function() { 
-  AudioPlayer.list();  
-//AudioPlayer.play(new Track("sample", 'Sample Music/sample.wav'));
-  console.log("OK =>  Audioplayer Play method called");
-});
+var queueAndPlay = function (playlistNum, trackNum) {
+    playlistNum = playlistNum || 0;
+    trackNum = trackNum || 0;
+    mopidy.playlists.getPlaylists()
+        // => list of Playlists
+        .fold(get, playlistNum)
+        // => Playlist
+        .then(printTypeAndName)
+        // => Playlist
+        .fold(get, 'tracks')
+        // => list of Tracks
+        .then(mopidy.tracklist.add)
+        // => list of TlTracks
+        .fold(get, trackNum)
+        // => TlTrack
+        .then(mopidy.playback.play)
+        // => null
+        .then(printNowPlaying)
+        // => null
+        .catch(console.error.bind(console))  // Handle errors here
+        // => null
+        .done();                       // ...or they'll be thrown here
+};
 
-
+var mopidy = new Mopidy();             // Connect to server
+mopidy.on(console.log.bind(console));  // Log all events
+mopidy.on("state:online", queueAndPlay);
