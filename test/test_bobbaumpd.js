ef156d8d9d68cca4ab31b7d42f1b59f68a3e1cfb
javier_canino <jcaninov@gmail.com>
2016-11-30 14:49:20 +0100

    Fixed bobboau/mpdjs to work with nodejs net socket.. Gloria bendita!

--- test/test_bobbaumpd.js
+++ test/test_bobbaumpd.js
@@ -2,6 +2,70 @@ var MPD = require("../lib/boubbou_mpd.js");
 
 var mpd_client = new MPD(6600, 'localhost');
 
-mpd_client.on('StateChanged',function(state){
-    console.log(state);
+//mpd_client.enableLogging();
+
+
+// 'Error', 'Event', 'UnhandledEvent', 'AuthFailure', 
+// 'DatabaseChanging', 'DataLoaded', 'OutputChanged', 
+// 'StateChanged', 'QueueChanged', 'PlaylistsChanged', 
+// 'PlaylistChanged','Connect', 'Disconnect'
+
+mpd_client.on('Connect',function(state){
+	var playlists = mpd_client.getPlaylists();
+	console.log("playlists: ");
+	console.log(playlists);
+
+	console.log("getPlaystate() => ");
+	console.log(mpd_client.getPlaystate());
+	mpd_client.loadPlaylistIntoQueue(playlists[0]);
+	mpd_client.play();
 });
+
+
+mpd_client.on('StateChanged',function(state){
+    console.log("--------------------------");    
+    console.log(state.playstate);
+    if (state.playstate == "play")
+    {
+    	var song = mpd_client.getCurrentSong();
+    	console.log("SONG => ");    
+    	console.log(song);
+	}
+});
+
+
+/*
+---------STATE Object------------
+{ version: '0.17.0',
+  connected: true,
+  playstate: 'play',
+  volume: 0.5,
+  repeat: 0,
+  single: 0,
+  consume: 0,
+  random: 0,
+  crossfade: 0,
+  mix_ramp_threshold: 0,
+  current_song: { queue_idx: 0, elapsed_time: 0, id: 2 },
+  next_song: { queue_idx: 1, id: 3 },
+  current_queue:
+   { addSongByFile: [Function],
+     clear: [Function],
+     removeSongByPosition: [Function],
+     moveSongByPosition: [Function],
+     swapSongsByPosition: [Function],
+     getSongs: [Function],
+     clone: [Function] },
+  queue_version: 7,
+  playlists:
+   [ { playlist: 'sample_playlist',
+       last_modified: '2016-11-29T09:49:36.505Z' },
+     { playlist: 'testplaylist_0801_2',
+       last_modified: '2016-11-29T08:44:31.505Z' } ],
+  playlistlength: 2,
+  mixrampdelay: '1.#QNAN0',
+  time: '0:215',
+  bitrate: 128,
+  audio: '44100:24:2' }
+--------------------------
+*/
